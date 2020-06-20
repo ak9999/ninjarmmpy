4635ec6c48e85df227a38c969f461293fbb61286
Abdullah Khan <abdullah@ajkhan.me>
2020-06-19 22:46:41 -0400

    Utility functions for web requests

--- ninjarmmpy/utils.py
+++ ninjarmmpy/utils.py
@@ -1,7 +1,8 @@
 from functools import wraps
+import requests
+from ninjarmmpy.auth import NinjaAuthentication
 
 def return_response(fn):
-    __doc__ = '''Decorator for generally getting a Response.text object'''
     @wraps(fn)
     def wrapped(*args, **kwargs):
         response = fn(*args, **kwargs)
@@ -9,3 +10,17 @@ def return_response(fn):
             return response.status_code
         return response.text
     return wrapped
+
+
+def api_get_request(self, url, params: dict = {}):
+    # params can't be used because functions break with them.
+    api_url = self.base_url + url
+    response = requests.get(url=api_url, auth=NinjaAuthentication(self.key, self.secret), params=params)
+    return response
+
+
+def api_post_request(self, url, params: dict = {}):
+    # params can't be used because functions break with them.
+    api_url = self.base_url + url
+    response = requests.post(url=api_url, auth=NinjaAuthentication(self.key, self.secret), params=params)
+    return response
