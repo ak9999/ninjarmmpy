7c443a6658d3ebffa71388b648085577cd90ea6c
Abdullah Khan <abdullah@ajkhan.me>
2020-06-20 22:37:36 -0400

    Amend client
    Inherits all the mixins

--- ninjarmmpy/client.py
+++ ninjarmmpy/client.py
@@ -2,9 +2,10 @@ from ninjarmmpy.system import SystemMixin
 from ninjarmmpy.organization import OrganizationMixin
 from ninjarmmpy.groups import GroupsMixin
 from ninjarmmpy.device import DeviceMixin
+# from ninjarmmpy.queries import QueriesMixin
 from ninjarmmpy.auth import NinjaAuthentication
 
-
+# class Client(SystemMixin, OrganizationMixin, GroupsMixin, DeviceMixin, QueriesMixin)
 class Client(SystemMixin, OrganizationMixin, GroupsMixin, DeviceMixin):
     NINJA_API_US_BASE_URL = 'https://api.ninjarmm.com'
     NINJA_API_EU_BASE_URL = 'https://eu-api.ninjarmm.com'
