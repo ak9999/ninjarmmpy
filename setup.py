629f71c3806023fa276648a592aeeaca6f32756d
Abdullah Khan <abdullah@ajkhan.me>
2020-06-22 09:30:25 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ninjarmmpy-ajkhan",
+    version="0.0.5",
+    author="Abdullah 'AJ' Khan",
+    author_email="aj@ajkhan.me",
+    description="Unofficial API client wrapper around v2 of the NinjaRMM API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ak9999/ninjarmmpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
