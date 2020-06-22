07c22a81967ffea82e5eb5f3d9b13db7eae11c8d
Abdullah Khan <abdullah@ajkhan.me>
2020-06-22 19:54:08 -0400

    Version bump due to package upload to PyPI

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ninjarmmpy-ajkhan",
-    version="0.0.5",
+    name="ninjarmmpy",
+    version="0.0.6",
     author="Abdullah 'AJ' Khan",
     author_email="aj@ajkhan.me",
     description="Unofficial API client wrapper around v2 of the NinjaRMM API",
