39573d79efac0198dcd2dbfa199b51555301f21b
Abdullah Khan <abdullah@ajkhan.me>
2020-08-03 22:21:50 -0400

    Define __repr__ for Client

--- ninjarmmpy/client.py
+++ ninjarmmpy/client.py
@@ -12,4 +12,8 @@ class Client(SystemMixin, OrganizationMixin, GroupsMixin, DeviceMixin, QueriesMi
     def __init__(self, AccessKeyID: str, SecretAccessKey: str, Europe=False):
         self.key = AccessKeyID
         self.secret = SecretAccessKey
-        self.base_url = self.NINJA_API_US_BASE_URL if not Europe else self.NINJA_API_EU_BASE_URL
+        self.europe = Europe
+        self.base_url = self.NINJA_API_US_BASE_URL if not self.europe else self.NINJA_API_EU_BASE_URL
+
+    def __repr__(self):
+        return f'{type(self).__name__}(AccessKeyID={self.key!r}, SecretAccessKey={self.secret!r}, Europe={self.europe!r})'
