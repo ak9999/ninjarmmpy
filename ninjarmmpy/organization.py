37c1f7edc720d28a6fcfc6c7337c062495ae8812
Abdullah Khan <abdullah@ajkhan.me>
2021-01-18 18:51:57 -0500

    Rename functions to match NinjaRMM API docs

--- ninjarmmpy/organization.py
+++ ninjarmmpy/organization.py
@@ -11,10 +11,20 @@ class OrganizationMixin():
         pass
 
     # Organization info
-    @return_response
-    def get_organization_devices(self, id: int = None, pageSize: int = None, after: int = None):
-        """Returns list of organizations (Brief mode)
 
+    @return_response
+    def getOrganization(self, id: int = None):
+        """Returns organization details (policy mapping, locations)
+        Keyword arguments:
+        id: int       -- Organization identifier, required, no default provided.
+        """
+        if not id:
+            raise ValueError('id needs to be set to an organizational identifier.')
+        return self.api_get_request(f'{self.NINJA_API_ORGANIZATION}{id}')
+
+    @return_response
+    def getOrganizationDevices(self, id: int = None, pageSize: int = None, after: int = None):
+        """Returns list of organizations (Brief mode)
         Keyword arguments:
         id: int       -- Organization identifier, required, no default provided.
         pageSize: int -- Limit number of organizations to return
@@ -26,9 +36,8 @@ class OrganizationMixin():
         return self.api_get_request(f'{self.NINJA_API_ORGANIZATION}{id}/devices', params=params)
 
     @return_response
-    def get_organization_locations(self, id: int = None):
+    def getOrganizationLocations(self, id: int = None):
         """Returns list of locations for organization
-
         Keyword arguments:
         id: int       -- Organization identifier, required, no default provided.
         """
@@ -37,20 +46,8 @@ class OrganizationMixin():
         return self.api_get_request(f'{self.NINJA_API_ORGANIZATION}{id}/locations')
 
     @return_response
-    def get_organization_details(self, id: int = None):
-        """Returns organization details (policy mapping, locations)
-
-        Keyword arguments:
-        id: int       -- Organization identifier, required, no default provided.
-        """
-        if not id:
-            raise ValueError('id needs to be set to an organizational identifier.')
-        return self.api_get_request(f'{self.NINJA_API_ORGANIZATION}{id}')
-
-    @return_response
-    def get_organization_endusers(self, id: int = None):
+    def getEndUsers(self, id: int = None):
         """Returns list of end-users for organization
-
         Keyword arguments:
         id: int       -- Organization identifier, required, no default provided.
         """
